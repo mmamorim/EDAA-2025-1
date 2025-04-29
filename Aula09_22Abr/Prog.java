67078c0527ea847a3ffc75fe874c95cda4151a7a
mmamorim <marc.m.amorim@gmail.com>
2025-04-29 09:59:48 -0300

    v1

--- Aula09_22Abr/Prog.java
+++ Aula09_22Abr/Prog.java
@@ -2,10 +2,15 @@
 public class Prog {
 
     public static void main(String[] args) {
-        int[] numbers = {8,3,5,1,4,9};
+        int[] numbers = {8,3,5,1,4,9,13,2,7};
 
-        bolha(numbers);
+        imprime(numbers);
+        insercao(numbers);
+        imprime(numbers);
 
+    }    
+
+    public static void imprime(int numbers[]) {
         System.out.print("[");
         for(int i=0; i<numbers.length;i++) {
             System.out.print(numbers[i]);
@@ -13,8 +18,8 @@ public class Prog {
                 System.out.print(",");
             }
         }
-        System.out.print("]");
-    }    
+        System.out.println("]");
+    }
 
     public static void bolha(int[] vetor) {
         int aux;
@@ -31,6 +36,39 @@ public class Prog {
             if(!trocou) {
                 break;
             }
+        }
+    }
+
+    public static void selecao(int[] vetor) {
+        int posMaior, aux;
+        for(int i=0; i< vetor.length-1; i++) {
+            posMaior = 0;
+            for(int k=1; k < vetor.length-i; k++) {
+                if(vetor[k] > vetor[posMaior]) {
+                    posMaior = k;
+                }
+            }
+            //System.out.println("trocando maior "+vetor[posMaior]+" com a posição "+(vetor.length-i-1));
+            aux = vetor[vetor.length-i-1];
+            vetor[vetor.length-i-1] = vetor[posMaior];
+            vetor[posMaior] = aux;
+        }
+    }
+
+    public static void insercao(int[] vetor) {
+        int k, aux;
+        for(int i=1; i< vetor.length; i++) {
+            //System.out.println("onde encaixar o elemento posição "+i);
+            aux = vetor[i];
+            for(k=i; k > 0; k--) {
+                if(aux < vetor[k-1]) {
+                    vetor[k] = vetor[k-1];
+                } else {
+                    break;
+                }
+            }
+            //System.out.println("posicionando elemento em "+k);
+            vetor[k] = aux;
         }
     }
 }
--- Aula09_22Abr/readme.md
+++ Aula09_22Abr/readme.md
@@ -21,3 +21,44 @@ public static void bolha(int[] vetor) {
     }
 }
 ~~~
+
+## Seleção
+
+~~~java
+public static void selecao(int[] vetor) {
+    int posMaior, aux;
+    for(int i=0; i< vetor.length-1; i++) {
+        posMaior = 0;
+        for(int k=1; k < vetor.length-i; k++) {
+            if(vetor[k] > vetor[posMaior]) {
+                posMaior = k;
+            }
+        }
+        //System.out.println("trocando maior "+vetor[posMaior]+" com a posição "+(vetor.length-i-1));
+        aux = vetor[vetor.length-i-1];
+        vetor[vetor.length-i-1] = vetor[posMaior];
+        vetor[posMaior] = aux;
+    }
+}
+~~~
+
+## Inserção
+
+~~~java
+public static void insercao(int[] vetor) {
+    int k, aux;
+    for(int i=1; i< vetor.length; i++) {
+        //System.out.println("onde encaixar o elemento posição "+i);
+        aux = vetor[i];
+        for(k=i; k > 0; k--) {
+            if(aux < vetor[k-1]) {
+                vetor[k] = vetor[k-1];
+            } else {
+                break;
+            }
+        }
+        //System.out.println("posicionando elemento em "+k);
+        vetor[k] = aux;
+    }
+}
+~~~
